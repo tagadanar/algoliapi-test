5f38072faf2e4ce5b3d039119df23366257ceada
raphael.renon@metronlab.com <raphael.renon@metronlab.com>
2018-05-28 14:34:45 +0200

    cleaning comments & documentation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, jsonify
@@ -6,32 +7,36 @@ from functools import reduce
 from time import time
 from dictionnaries import Vividict, Counterdict
 
-# setting up Flask app
+# Setting up Flask app
 app = Flask(__name__)
-# setting up global logs dict
+# Setting up global logs dict
 logs = Vividict()
 
 
-# utility functions
-def load_logs():
-    print("loading... please wait.")
+# Utility functions
+def load_logs(filename):
+    print("loading, please wait...")
     start = time()
-    with open("hn_logs.tsv", 'r') as f:
+    with open(filename, 'r') as f:
         for line in f:
             splited = line.strip('\n').split('\t')
             date = datetime(*map(int, splited[0].replace(":", "-").replace(" ", "-").split("-")))
             url = splited[1]
             add_to_logs(date, url)
     end = time()
-    print( str(round(end - start, 3)) + "s")
+    print("loaded in : " + str(round(end - start, 3)) + "s")
+
 
 def add_to_logs(date, url):
+    """ Aggregate url counter on a minute step """
     counterdict = logs[date.year][date.month][date.day][date.hour][date.minute]
     if not isinstance(counterdict, Counterdict):
         counterdict = logs[date.year][date.month][date.day][date.hour][date.minute] = Counterdict()
     counterdict[url] += 1
 
+
 def recurse_iter(d):
+    """ Generator that iterate over nested Vividict d and yield on Counterdict a (key, value) tuple """
     if isinstance(d, Vividict):
         for k in d:
             for rv in recurse_iter(d[k]):
@@ -40,21 +45,25 @@ def recurse_iter(d):
         for k, v in d.items():
             yield (k, v)
 
+
 def get_nested_logs(date_str):
+    """ Return nested Vividict in logs according to date_str """
     date_array = map(int, date_str.replace(":", "-").replace(" ", "-").split("-"))
-    nested = reduce(lambda a,b: a[b], date_array, logs)
+    nested = reduce(lambda a, b: a[b], date_array, logs)
     return nested
 
+
 def sum_nested_logs(nested):
+    """ Sum aggregated url counter of the nested Vividict """
     result = Counterdict()
     for url, nb_hit in recurse_iter(nested):
         result[url] += nb_hit
     return result
 
+
 def get_most_popular(d, n):
+    """ Return the n most popular query inside nested dict d """
     return dict(sorted(d.items(), key=lambda e: e[1], reverse=True)[:n])
-
-
 
 
 @app.route('/1/queries/count/<date_prefix>', methods=['GET'])
@@ -62,7 +71,8 @@ def count(date_prefix=None):
     nested_logs = get_nested_logs(date_prefix)
     summed_logs = sum_nested_logs(nested_logs)
     count = sum(1 for i in summed_logs)
-    return jsonify({ "count": count })
+    return jsonify({"count": count})
+
 
 @app.route('/1/queries/popular/<date_prefix>', methods=['GET'])
 def popular(date_prefix=None):
@@ -70,15 +80,15 @@ def popular(date_prefix=None):
     nested_logs = get_nested_logs(date_prefix)
     summed_logs = sum_nested_logs(nested_logs)
     result = get_most_popular(summed_logs, size)
-    json = { "queries": []}
+    json = {"queries": []}
     for query, count in result.items():
-        json["queries"].append({"query":query, "count":count})
+        json["queries"].append({"query": query, "count": count})
 
     return jsonify(json)
 
 
 # LOADING LOGS
-load_logs()
+load_logs("hn_logs.tsv")
 if __name__ == '__main__':
-    # LAUNCHING REST API
+    # LAUNCHING REST API
     app.run(host='0.0.0.0', port=5000, debug=False)
--- dictionnaries.py
+++ dictionnaries.py
@@ -1,14 +1,14 @@
 class Vividict(dict):
     """
     autovivified dictionnary, always have a Vividict as value on new keys
-    https://stackoverflow.com/questions/635483/
+    more info : https://stackoverflow.com/questions/635483/
     """
     def __missing__(self, key):
-        value = self[key] = type(self)() # retain local pointer to value
+        value = self[key] = type(self)()
         return value
 
 class Counterdict(dict):
-    """ dict that always have an int 0 as value """
+    """ dict that always have an int 0 as value on new keys """
     def __missing__(self, key):
         value = self[key] = 0
         return value
